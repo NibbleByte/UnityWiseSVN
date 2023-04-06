9a61fb4d98382d4a24b05411a73b06e8f8f3c7cd
Filip Slavov <NibbleByte3@gmail.com>
2023-04-06 15:36:40 +0300

    Fixed #5 - support SnailSVN premium protocol as well: "snailsvnfree://" vs "snailsvn://"

--- Editor/ContextMenus/SnailSVNContextMenus.cs
+++ Editor/ContextMenus/SnailSVNContextMenus.cs
@@ -20,6 +20,9 @@ namespace DevLocker.VersionControl.WiseSVN.ContextMenus.Implementation
 		private const string ClientLightCommand =	"/Applications/SnailSVNLite.app/Contents/Resources/snailsvn.sh";
 		private const string ClientPremiumCommand = "/Applications/SnailSVN.app/Contents/Resources/snailsvn.sh";
 
+		private const string ClientLightProtocol = "snailsvnfree";
+		private const string ClientPremiumProtocol = "snailsvn";
+
 		protected override string FileArgumentsSeparator => " ";
 		protected override bool FileArgumentsSurroundQuotes => true;
 
@@ -39,6 +42,8 @@ namespace DevLocker.VersionControl.WiseSVN.ContextMenus.Implementation
 			});
 		}
 
+		private string GetClientProtocol() => File.Exists(ClientPremiumCommand) ? ClientPremiumProtocol : ClientLightProtocol;
+
 		public override void CheckChanges(IEnumerable<string> assetPaths, bool includeMeta, bool wait = false)
 		{
 			if (!assetPaths.Any())
@@ -50,7 +55,7 @@ namespace DevLocker.VersionControl.WiseSVN.ContextMenus.Implementation
 
 			// The snailsvn.sh currently doesn't accept "check-for-modifications" argument, but with some reverse engineering, managed to make it work like this.
 			// open "snailsvnfree://check-for-modifications/SomeFolderHere/UnityProject/Assets"
-			Application.OpenURL($"snailsvnfree://check-for-modifications{WiseSVNIntegration.ProjectRootNative}/{path}");
+			Application.OpenURL($"{GetClientProtocol()}://check-for-modifications{WiseSVNIntegration.ProjectRootNative}/{path}");
 		}
 
 		public override void DiffChanges(string assetPath, bool wait = false)
@@ -172,7 +177,7 @@ namespace DevLocker.VersionControl.WiseSVN.ContextMenus.Implementation
 
 			// The snailsvn.sh currently doesn't accept "blame" argument, but with some reverse engineering, managed to make it work like this.
 			// open "snailsvnfree://svn-blame/SomeFolderHere/UnityProject/Assets/foo.txt"
-			Application.OpenURL($"snailsvnfree://svn-blame{WiseSVNIntegration.ProjectRootNative}/{assetPath}");
+			Application.OpenURL($"{GetClientProtocol()}://svn-blame{WiseSVNIntegration.ProjectRootNative}/{assetPath}");
 		}
 
 		public override void Cleanup(bool wait = false)
