d8bfdcc6c2e53d6af58523904841455dfa950198
Filip Slavov <NibbleByte3@gmail.com>
2023-05-13 12:42:32 +0300

    Fixed #6 - added CLI context menu support (editor window to run svn commands). Used on Linux

---
+++ Assets/DevLocker/VersionControl/WiseSVN/Editor/ContextMenus/CLIContextMenus.cs
@@ -0,0 +1,192 @@
+// MIT License Copyright(c) 2022 Filip Slavov, https://github.com/NibbleByte/UnityWiseSVN
+
+using DevLocker.VersionControl.WiseSVN.Shell;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace DevLocker.VersionControl.WiseSVN.ContextMenus.Implementation
+{
+#if UNITY_EDITOR_WIN
+	//
+	internal class CLIContextMenus : SVNContextMenusBase
+	{
+		protected override string FileArgumentsSeparator => "\n";
+		protected override bool FileArgumentsSurroundQuotes => true;
+
+		public override void CheckChanges(IEnumerable<string> assetPaths, bool includeMeta, bool wait = false)
+		{
+			if (!assetPaths.Any())
+				return;
+
+			string pathsArg = AssetPathsToContextPaths(assetPaths, includeMeta);
+			if (string.IsNullOrEmpty(pathsArg))
+				return;
+
+			CLIContextWindow.Show($"diff \n{pathsArg}", true);
+		}
+
+		public override void DiffChanges(string assetPath, bool wait = false)
+		{
+			string pathsArg = AssetPathToContextPaths(assetPath, false);
+			if (string.IsNullOrEmpty(pathsArg))
+				return;
+
+			CLIContextWindow.Show($"diff \n{pathsArg}", true);
+		}
+
+		public override void Update(IEnumerable<string> assetPaths, bool includeMeta, bool wait = false)
+		{
+			if (!assetPaths.Any())
+				return;
+
+			// NOTE: @ is added at the end of path, to avoid problems when file name contains @, and SVN mistakes that as "At revision" syntax".
+			//		https://stackoverflow.com/questions/757435/how-to-escape-characters-in-subversion-managed-file-names
+
+			string pathsArg = AssetPathsToContextPaths(assetPaths, includeMeta);
+			if (string.IsNullOrEmpty(pathsArg))
+				return;
+
+			CLIContextWindow.Show($"update --depth infinity --accept postpone\n{pathsArg}", true);
+		}
+
+		public override void Commit(IEnumerable<string> assetPaths, bool includeMeta, bool wait = false)
+		{
+			if (!assetPaths.Any())
+				return;
+
+			string pathsArg = AssetPathsToContextPaths(assetPaths, includeMeta);
+			if (string.IsNullOrEmpty(pathsArg))
+				return;
+
+			CLIContextWindow.Show($"commit --depth infinity -m \"\"\n{pathsArg}", false);
+		}
+
+
+
+		public override void Add(IEnumerable<string> assetPaths, bool includeMeta, bool wait = false)
+		{
+			if (!assetPaths.Any())
+				return;
+
+			foreach (var path in assetPaths) {
+				if (!WiseSVNIntegration.CheckAndAddParentFolderIfNeeded(path, true))
+					return;
+			}
+
+			// Don't give versioned metas, as tortoiseSVN doesn't like it.
+			var metas = assetPaths
+				.Select(path => path + ".meta")
+				.Where(path => WiseSVNIntegration.GetStatus(path).Status == VCFileStatus.Unversioned)
+				;
+
+			string pathsArg = AssetPathsToContextPaths(includeMeta ? assetPaths.Concat(metas) : assetPaths, false);
+			if (string.IsNullOrEmpty(pathsArg))
+				return;
+
+			CLIContextWindow.Show($"add --depth infinity\n{pathsArg}", true);
+		}
+
+		public override void Revert(IEnumerable<string> assetPaths, bool includeMeta, bool wait = false)
+		{
+			if (!assetPaths.Any())
+				return;
+
+			string pathsArg = AssetPathsToContextPaths(assetPaths, includeMeta);
+			if (string.IsNullOrEmpty(pathsArg))
+				return;
+
+			CLIContextWindow.Show($"revert --depth infinity\n{pathsArg}", false);
+		}
+
+
+
+		public override void ResolveAll(bool wait = false)
+		{
+			CLIContextWindow.Show($"resolve --depth infinity --accept TYPE_IN_OPTION\n", false);
+		}
+
+		public override void Resolve(string assetPath, bool wait = false)
+		{
+			DiffChanges(assetPath, wait);
+		}
+
+
+
+		public override void GetLocks(IEnumerable<string> assetPaths, bool includeMeta, bool wait = false)
+		{
+			if (!assetPaths.Any())
+				return;
+
+			string pathsArg = AssetPathsToContextPaths(assetPaths, includeMeta);
+			if (string.IsNullOrEmpty(pathsArg))
+				return;
+
+			CLIContextWindow.Show($"lock -m \"\"\n{pathsArg}", false);
+		}
+
+		public override void ReleaseLocks(IEnumerable<string> assetPaths, bool includeMeta, bool wait = false)
+		{
+			if (!assetPaths.Any())
+				return;
+
+			string pathsArg = AssetPathsToContextPaths(assetPaths, includeMeta);
+			if (string.IsNullOrEmpty(pathsArg))
+				return;
+
+			CLIContextWindow.Show($"unlock\n{pathsArg}", true);
+		}
+
+		public override void ShowLog(string assetPath, bool wait = false)
+		{
+			if (string.IsNullOrEmpty(assetPath))
+				return;
+
+			string pathsArg = AssetPathToContextPaths(assetPath, false);
+			if (string.IsNullOrEmpty(pathsArg))
+				return;
+
+			CLIContextWindow.Show($"log \n{pathsArg}", true);
+		}
+
+
+
+		public override void Blame(string assetPath, bool wait = false)
+		{
+			if (string.IsNullOrEmpty(assetPath))
+				return;
+
+			string pathsArg = AssetPathToContextPaths(assetPath, false);
+			if (string.IsNullOrEmpty(pathsArg))
+				return;
+
+			CLIContextWindow.Show($"blame \n{pathsArg}", true);
+		}
+
+
+
+		public override void Cleanup(bool wait = false)
+		{
+			CLIContextWindow.Show($"cleanup", true);
+		}
+
+
+		public override void RepoBrowser(string url, bool wait = false)
+		{
+			if (string.IsNullOrEmpty(url))
+				return;
+
+			CLIContextWindow.Show($"list\n\"{url}\"", true);
+		}
+
+		public override void Switch(string localPath, string url, bool wait = false)
+		{
+			if (string.IsNullOrEmpty(localPath) || string.IsNullOrEmpty(url))
+				return;
+
+			// TODO: Test?
+			CLIContextWindow.Show($"switch\n\"{url}\"\n\"{localPath}\"", false);
+		}
+	}
+#endif
+}
---
+++ Assets/DevLocker/VersionControl/WiseSVN/Editor/ContextMenus/CLIContextWindow.cs
@@ -0,0 +1,201 @@
+// MIT License Copyright(c) 2022 Filip Slavov, https://github.com/NibbleByte/UnityWiseSVN
+
+#if UNITY_2020_2_OR_NEWER || UNITY_2019_4_OR_NEWER || (UNITY_2018_4_OR_NEWER && !UNITY_2018_4_19 && !UNITY_2018_4_18 && !UNITY_2018_4_17 && !UNITY_2018_4_16 && !UNITY_2018_4_15)
+#define CAN_DISABLE_REFRESH
+#endif
+
+using DevLocker.VersionControl.WiseSVN.Shell;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace DevLocker.VersionControl.WiseSVN.ContextMenus.Implementation
+{
+#if UNITY_EDITOR
+	/// <summary>
+	/// Window to display SVN command that is about to be executed. User can tweak it.
+	/// </summary>
+	public class CLIContextWindow : EditorWindow
+	{
+		private bool m_AutoRun;
+		private string m_CommandArgs;
+		private Vector2 m_CommandArgsScroll;
+
+		private string m_CombinedOutput = "";
+		private string m_StateLabel = "Idle";
+		private Color m_StateColor = Color.white;
+		private Vector2 m_OutputScroll;
+
+		private bool m_IsWorking => m_SVNOperation != null && !m_SVNOperation.HasFinished;
+
+
+		private SVNAsyncOperation<ShellUtils.ShellResult> m_SVNOperation;
+
+		public static void Show(string commandArgs, bool autoRun)
+		{
+			var window = CreateInstance<CLIContextWindow>();
+
+			window.position = new Rect(window.position.xMin + 100f, window.position.yMin + 100f, 700f, 600f);
+			window.minSize = new Vector2(700f, 400f);
+			window.titleContent = new GUIContent("SVN CLI Window");
+
+			window.m_CommandArgs = commandArgs;
+			window.m_AutoRun = autoRun;
+			window.ShowUtility();
+		}
+
+		void OnEnable()
+		{
+#if CAN_DISABLE_REFRESH
+			AssetDatabase.DisallowAutoRefresh();
+#endif
+		}
+
+		void OnDisable()
+		{
+#if CAN_DISABLE_REFRESH
+			AssetDatabase.AllowAutoRefresh();
+			AssetDatabase.Refresh();
+#endif
+		}
+
+		void OnDestroy()
+		{
+			if (m_IsWorking) {
+				m_SVNOperation.Abort(true);
+			}
+		}
+
+		void OnGUI()
+		{
+			var textAreaStyle = new GUIStyle(EditorStyles.textArea);
+			textAreaStyle.wordWrap = false;
+			Color prevColor = GUI.color;
+
+			EditorGUI.BeginDisabledGroup(true);
+			{
+				EditorGUILayout.TextField("Command", "svn");
+			}
+			EditorGUI.EndDisabledGroup();
+
+			EditorGUI.BeginDisabledGroup(m_IsWorking);
+			{
+				var textSize = textAreaStyle.CalcSize(new GUIContent(m_CommandArgs));
+				m_CommandArgsScroll = EditorGUILayout.BeginScrollView(m_CommandArgsScroll, GUILayout.Height(100f));
+				m_CommandArgs = EditorGUILayout.TextArea(m_CommandArgs, textAreaStyle, GUILayout.ExpandHeight(true), GUILayout.ExpandWidth(true), GUILayout.MinWidth(textSize.x), GUILayout.MinHeight(textSize.y));
+				EditorGUILayout.EndScrollView();
+			}
+			EditorGUI.EndDisabledGroup();
+
+
+			EditorGUILayout.LabelField("Output:");
+			m_OutputScroll = EditorGUILayout.BeginScrollView(m_OutputScroll);
+			EditorGUILayout.TextArea(m_CombinedOutput, textAreaStyle, GUILayout.ExpandHeight(true));
+			EditorGUILayout.EndScrollView();
+
+
+
+
+			EditorGUILayout.BeginHorizontal();
+			{
+				EditorGUI.BeginDisabledGroup(!m_IsWorking);
+
+				if (GUILayout.Button("Abort")) {
+					m_SVNOperation.Abort(false);
+					m_CombinedOutput += "Aborting...\n";
+					m_StateLabel = "Aborting...";
+					m_StateColor = Color.red;
+					GUI.FocusControl("");
+				}
+
+				if (GUILayout.Button("Kill")) {
+					m_SVNOperation.Abort(true);
+					m_CombinedOutput += "Killing...\n";
+					m_StateLabel = "Killing...";
+					m_StateColor = Color.red;
+					GUI.FocusControl("");
+				}
+
+				EditorGUI.EndDisabledGroup();
+
+				GUILayout.Space(8f);
+
+				GUI.color = m_StateColor;
+				EditorGUILayout.LabelField(m_StateLabel, GUILayout.ExpandWidth(false));
+				GUI.color = prevColor;
+
+				GUILayout.FlexibleSpace();
+
+				GUI.color = Color.yellow;
+				GUILayout.Label("Assets Auto Refresh Disabled!", GUILayout.ExpandWidth(false));
+				GUI.color = prevColor;
+
+				if (GUILayout.Button("Clear Output")) {
+					m_CombinedOutput = "";
+					GUI.FocusControl("");
+				}
+				if (GUILayout.Button("Copy Output")) {
+					GUIUtility.systemCopyBuffer = m_CombinedOutput;
+					GUI.FocusControl("");
+				}
+
+				EditorGUI.BeginDisabledGroup(m_IsWorking || string.IsNullOrWhiteSpace(m_CommandArgs));
+
+				GUILayout.Space(8f);
+
+
+				if (Event.current.shift) {
+					m_AutoRun = false;
+				}
+
+				if (GUILayout.Button("Run") || m_AutoRun) {
+					m_AutoRun = false;
+					GUI.FocusControl("");
+					m_CommandArgs = m_CommandArgs.Trim();
+					m_SVNOperation = SVNAsyncOperation<ShellUtils.ShellResult>.Start(
+						op => ShellUtils.ExecuteCommand("svn", m_CommandArgs.Replace("\n", " "), op)
+						);
+
+					m_SVNOperation.AnyOutput += (line) => {
+						m_CombinedOutput += line + "\n";
+
+						// In case window got closed.
+						if (this) {
+							Repaint();
+						}
+					};
+
+					m_SVNOperation.Completed += (op) => {
+						if (op.Result.HasErrors) {
+							m_StateLabel = "Failed!";
+							m_StateColor = Color.red;
+						} else {
+							m_StateLabel = op.AbortRequested ? "Aborted!" : "Completed!";
+							m_StateColor = op.AbortRequested ? Color.red : Color.green;
+						}
+
+						m_SVNOperation = null;
+						m_CombinedOutput += "\n";
+
+						// In case window got closed.
+						if (this) {
+							SVNStatusesDatabase.Instance.InvalidateDatabase();
+							Repaint();
+						}
+					};
+
+					m_StateLabel = "Working...";
+					m_StateColor = Color.yellow;
+				}
+
+				EditorGUI.EndDisabledGroup();
+			}
+			EditorGUILayout.EndHorizontal();
+		}
+	}
+#endif
+}
