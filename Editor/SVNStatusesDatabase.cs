a2e84ff49ad70809fcd53dbea11524f0bf85a2ac
Filip Slavov <NibbleByte3@gmail.com>
2023-09-12 22:48:43 +0300

    * Fixed: recursive externals displayed false modified "!" overlay icon. https://forum.unity.com/threads/wise-svn-powerful-tortoisesvn-snailsvn-integration.844168/page-2#post-9287362

--- Editor/SVNStatusesDatabase.cs
+++ Editor/SVNStatusesDatabase.cs
@@ -520,7 +520,7 @@ namespace DevLocker.VersionControl.WiseSVN
 		private void AddModifiedFolders(SVNStatusData statusData)
 		{
 			var status = statusData.Status;
-			if (status == VCFileStatus.Unversioned || status == VCFileStatus.Ignored || status == VCFileStatus.Normal || status == VCFileStatus.Excluded)
+			if (status == VCFileStatus.Unversioned || status == VCFileStatus.Ignored || status == VCFileStatus.Normal || status == VCFileStatus.Excluded || status == VCFileStatus.External || status == VCFileStatus.ReadOnly)
 				return;
 
 			if (statusData.IsConflicted) {
