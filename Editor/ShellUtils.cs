a91f9f7c5795384bcd2f76bae7ea8222c34ca00a
NibbleByte <NibbleByte3@gmail.com>
2024-02-05 23:58:15 +0200

    * ShellUtils now returns error code & process id. Can check if process is till alive. * ShellUtils now properly waits for the output streams to be closed, after process died. Hope this fixes the hangs as well.

--- Editor/ShellUtils.cs
+++ Editor/ShellUtils.cs
@@ -35,7 +35,8 @@ namespace DevLocker.VersionControl.WiseSVN.Shell
 			public string Args;
 			public string WorkingDirectory;
 			public bool WaitForOutput;
-			public int WaitTimeout;		// WaitTimeout is in milliseconds. -1 means forever. Only if WaitForOutput is true.
+			public int WaitTimeout;     // WaitTimeout is in milliseconds. -1 means forever. Only if WaitForOutput is true.
+			public bool SkipTimeoutError;
 			public IShellMonitor Monitor;
 		}
 
@@ -46,14 +47,28 @@ namespace DevLocker.VersionControl.WiseSVN.Shell
 
 			public string Output;
 			public string Error;
+			public int ErrorCode;
 
-			public bool HasErrors => !string.IsNullOrEmpty(Error);
+			public int ProcessId;
+
+			public bool HasErrors => !string.IsNullOrWhiteSpace(Error) || ErrorCode != 0;
 		}
 
 		public const string USER_ABORTED_LOG = "User aborted the operation...";
 
 		// If result error message contains this string token, the operation was interrupted due to time out.
 		public const string TIME_OUT_ERROR_TOKEN = "[ERR_TIME_OUT]";
+
+		public static bool IsProcessAlive(int processId)
+		{
+			try {
+				return !Process.GetProcessById(processId)?.HasExited ?? false;
+
+			} catch (System.ArgumentException) {
+				// Process is missing - it finished. Do nothing.
+				return false;
+			}
+		}
 
 		public static ShellResult ExecuteCommand(string command, string args)
 		{
@@ -87,6 +102,18 @@ namespace DevLocker.VersionControl.WiseSVN.Shell
 			});
 		}
 
+		public static ShellResult ExecuteCommand(string command, string args, string workingDirectory, int waitTimeout, IShellMonitor monitor = null)
+		{
+			return ExecuteCommand(new ShellArgs() {
+				Command = command,
+				Args = args,
+				WaitForOutput = true,
+				WaitTimeout = waitTimeout,
+				WorkingDirectory = workingDirectory,
+				Monitor = monitor
+			});
+		}
+
 		public static ShellResult ExecuteCommand(ShellArgs shellArgs)
 		{
 			shellArgs.Command = shellArgs.Command ?? string.Empty;
@@ -99,6 +126,7 @@ namespace DevLocker.VersionControl.WiseSVN.Shell
 			result.Args = shellArgs.Args;
 			result.Output = string.Empty;
 			result.Error = string.Empty;
+			result.ErrorCode = 0;
 
 			if (shellArgs.Monitor != null) {
 
@@ -124,6 +152,8 @@ namespace DevLocker.VersionControl.WiseSVN.Shell
 				process = Process.Start(processStartInfo);
 				// Input is not supported so close it directly. Prevents hangs when CLI prompts the user for something (authentication for example).
 				process.StandardInput.Close();
+
+				result.ProcessId = process.Id;
 
 			} catch (System.Exception ex) {
 				// Most probably file not found.
@@ -172,6 +202,7 @@ namespace DevLocker.VersionControl.WiseSVN.Shell
 			//
 			StringBuilder outputBuilder = new StringBuilder();
 			DataReceivedEventHandler outputReadLineHandler = null;
+			bool outputEndReached = false;
 			outputReadLineHandler = (sender, args) => {
 				// Lock - check the Builder usage at the end.
 				lock (outputBuilder) {
@@ -180,17 +211,23 @@ namespace DevLocker.VersionControl.WiseSVN.Shell
 						if (shellArgs.Monitor != null) {
 							shellArgs.Monitor.AppendOutputLine(args.Data);
 						}
+
+					} else {
+						// End of stream reached
+						Volatile.Write(ref outputEndReached, true);	// Not sure if needed.
 					}
 				}
 			};
 			process.OutputDataReceived += outputReadLineHandler;
 			process.BeginOutputReadLine();
 
+
 			//
 			// Subscribe for error output.
 			//
 			StringBuilder errorBuilder = new StringBuilder();
 			DataReceivedEventHandler errorReadLineHandler = null;
+			bool errorEndReached = false;
 			errorReadLineHandler = (sender, args) => {
 				// Lock - check the Builder usage at the end.
 				lock (errorBuilder) {
@@ -199,6 +236,9 @@ namespace DevLocker.VersionControl.WiseSVN.Shell
 						if (shellArgs.Monitor != null) {
 							shellArgs.Monitor.AppendErrorLine(args.Data);
 						}
+					} else {
+						// End of stream reached
+						Volatile.Write(ref errorEndReached, true);	// Not sure if needed.
 					}
 				}
 			};
@@ -218,26 +258,47 @@ namespace DevLocker.VersionControl.WiseSVN.Shell
 				stopwatch.Stop();
 
 				// If process is still running, the timeout kicked in.
-				if (!process.HasExited) {
-					result.Error += $"Command [{shellArgs.Command} {shellArgs.Args}] timed out. {TIME_OUT_ERROR_TOKEN}";
+				if (!process.HasExited && !shellArgs.SkipTimeoutError) {
+					string timeoutError = $"Command [{shellArgs.Command} {shellArgs.Args}] timed out. {TIME_OUT_ERROR_TOKEN}";
+					if (shellArgs.Monitor != null) {
+						shellArgs.Monitor.AppendErrorLine(timeoutError);
+					}
+					result.Error += timeoutError;
+				}
+			}
+
+			int waitStreamTries = 0; // Keep it here for debugging, if Dispose() hangs.
+			if (process.HasExited) {
+				// Sometimes stream is still pending even when process has exited. Example: git status --porcelain -z "Assets"
+				// Wait a bit for the stream to catch-up.
+				// Receiving null means stream ended.
+				const int waitStreamTriesMax = 5;
+				for (; waitStreamTries < waitStreamTriesMax; ++waitStreamTries) {
+					Volatile.Read(ref outputEndReached);	// Not sure if needed.
+					Volatile.Read(ref errorEndReached);		// Not sure if needed.
+
+					if (outputEndReached && errorEndReached)
+						break;
+
+					Thread.Sleep(10);
 				}
 			}
 
 			process.CancelOutputRead();
 			process.OutputDataReceived -= outputReadLineHandler;
-			//process.StandardOutput.Close();
+			//process.StandardOutput.Close();	// Doesn't work with async event workflow.
 			lock (outputBuilder) {
 				// When the main thread gets here, the process will not be running (unless it timed out),
 				// but the OutputDataReceived thread might still be appending the final strings. Lock it!
-				result.Output = outputBuilder.ToString();
+				result.Output = outputBuilder.ToString().Replace("\r\n", "\n").Trim('\n');	// Don't trim spaces, they matter.
 			}
 
 			process.CancelErrorRead();
 			process.ErrorDataReceived -= errorReadLineHandler;
-			//process.StandardError.Close();
+			//process.StandardError.Close();	// Doesn't work with async event workflow.
 			lock (errorBuilder) {
 				// Same as above. Concat if error was present.
-				result.Error += errorBuilder.ToString();
+				result.Error += errorBuilder.ToString().Replace("\r\n", "\n").Trim('\n');
 			}
 
 			if (shellArgs.Monitor != null) {
@@ -253,6 +314,7 @@ namespace DevLocker.VersionControl.WiseSVN.Shell
 				// Example: x64 executable can't run on arm64 system.
 				if (string.IsNullOrEmpty(result.Error) && process.ExitCode != 0) {
 					result.Error = $"Process failed with exit code {process.ExitCode}.";
+					result.ErrorCode = process.ExitCode;
 
 					if (shellArgs.Monitor != null) {
 						shellArgs.Monitor.AppendErrorLine(result.Error);
@@ -260,15 +322,20 @@ namespace DevLocker.VersionControl.WiseSVN.Shell
 				}
 
 				// HACK: Dispose sometimes hangs for unknown reason. Hope this helps. Let me know if another hang is experienced.
-				if (string.IsNullOrWhiteSpace(result.Output) && string.IsNullOrWhiteSpace(result.Error)) {
-					Thread.Sleep(50);
-				}
+				// EDIT: Sleep didn't help - it got stuck on simple command GetWorkingBranchDivergingCommit(), while reloading assembly.
+				//if (string.IsNullOrWhiteSpace(result.Output) && string.IsNullOrWhiteSpace(result.Error)) {
+				//	Thread.Sleep(50);
+				//}
 
 				process.Dispose();
+
 				// This still hangs sometimes. Fixes tried:
 				// - added process.CancelOutputRead()
 				// - Sleep on empty result. I think it got stuck on empty result from GetStatus() for a file.
+				// - Wait for stream end for real!
 				// Useful article: https://newbedev.com/process-sometimes-hangs-while-waiting-for-exit
+				//
+				// UnityEditor.EditorApplication.isCompiling - Not thead-safe? Better not use it, although no exception.
 				//
 				// Last hang encountered: processing a lot of assets after migration of Unity version. Unity hang for a lot of time.
 				// Breaking in with debugger yielded this stack:
