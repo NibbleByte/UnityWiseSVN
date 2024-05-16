2c09fea19f5f824662ff38c1aec0675060f877b7
NibbleByte <NibbleByte3@gmail.com>
2024-05-16 23:44:44 +0300

    * Switched persistent singletons from using Resources.FindObjectsOfTypeAll() to simple constructor, like the ScriptableSingleton. Should speed up assembly reloads.

--- Editor/Utils/EditorPersistentSingleton.cs
+++ Editor/Utils/EditorPersistentSingleton.cs
@@ -18,33 +18,34 @@ namespace DevLocker.VersionControl.WiseSVN.Utils
 		where SingletonType : ScriptableObject, IEditorPersistentSingleton
 	{
 
+		private static bool m_SingletonInitialized;
 		private static SingletonType m_Instance;
+
 		public static SingletonType Instance {
 			get {
+				bool freshlyCreated = false;
+
 				if (m_Instance == null) {
-					m_Instance = Resources.FindObjectsOfTypeAll<SingletonType>().FirstOrDefault();
+					ScriptableObject.CreateInstance<SingletonType>(); // No need to assign it - the constructor will do it.
+					m_Instance.name = m_Instance.GetType().Name;	// nameof(SingletonType) returns "SingletonType".
 
-					bool freshlyCreated = false;
-					if (m_Instance == null) {
+					// Setting this flag will tell Unity NOT to destroy this object on assembly reload (as no scene references this object).
+					// We're essentially leaking this object. But we can still get a reference to it after reload,
+					// when Unity recreates the existing scriptable objects (this will call the object constructor during deserialization).
+					// More info on this: https://blogs.unity3d.com/2012/10/25/unity-serialization/
+					m_Instance.hideFlags = HideFlags.HideAndDontSave;
 
-						m_Instance = ScriptableObject.CreateInstance<SingletonType>();
-						m_Instance.name = m_Instance.GetType().Name;	// nameof(SingletonType) returns "SingletonType".
+					freshlyCreated = true;
+				}
 
-						// Setting this flag will tell Unity NOT to destroy this object on assembly reload (as no scene references this object).
-						// We're essentially leaking this object. But we can still find it with Resources.FindObjectsOfTypeAll() after reload.
-						// More info on this: https://blogs.unity3d.com/2012/10/25/unity-serialization/
-						m_Instance.hideFlags = HideFlags.HideAndDontSave;
+				// Data is already deserialized by Unity onto the scriptable object.
+				// Even though OnEnable is not yet called, data is there after assembly reload.
+				// It is deserialized even before static constructors [InitializeOnLoad] are called. I tested it! :D
 
-						freshlyCreated = true;
+				// The idea here is to save some time on assembly reload from deserializing json as the reload is already slow enough for big projects.
 
-					} else {
-						// Data is already deserialized by Unity onto the scriptable object.
-						// Even though OnEnable is not yet called, data is there after assembly reload.
-						// It is deserialized even before static constructors [InitializeOnLoad] are called. I tested it! :D
-
-						// The idea here is to save some time on assembly reload from deserializing json as the reload is already slow enough for big projects.
-					}
-
+				if (!m_SingletonInitialized || freshlyCreated) {	// Freshly created also, just in case?
+					m_SingletonInitialized = true;
 					m_Instance.Initialize(freshlyCreated);
 				}
 
@@ -52,6 +53,21 @@ namespace DevLocker.VersionControl.WiseSVN.Utils
 			}
 
 
+		}
+
+
+		protected EditorPersistentSingleton()
+		{
+			// Constructor will get called on calling Instance OR during deserialization on assembly reload.
+			// If this is assembly reload, fields will not be deserialized yet - initialize on demand, not here.
+			// This method was inspared by the Unity ScriptableSingleton:
+			// https://docs.unity3d.com/2020.1/Documentation/ScriptReference/ScriptableSingleton_1.html
+			if (m_Instance == null) {
+				m_Instance = this as SingletonType;
+
+			} else {
+				Debug.LogError($"{GetType().Name} singleton found another instance! Abort!", this);
+			}
 		}
 
 		public abstract void Initialize(bool freshlyCreated);
